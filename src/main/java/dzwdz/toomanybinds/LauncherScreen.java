c633a46f93c428361b1ee3a96e8d6415ddfc5c94
fanta <tobiasmuellner@outlook.com>
2022-12-11 11:46:02 +0100

    update 1.19.3

--- src/main/java/dzwdz/toomanybinds/LauncherScreen.java
+++ src/main/java/dzwdz/toomanybinds/LauncherScreen.java
@@ -137,7 +137,6 @@ public class LauncherScreen extends Screen {
         baseX = (width - w) / 2;
         baseY = (height - lineHeight) / 2;
 
-        client.keyboard.setRepeatEvents(true);
         String text = "";
         if (textField != null) text = textField.getText();
         textField = new TextFieldWidget(textRenderer, getX(), getY()+1, w, lineHeight, NarratorManager.EMPTY);
@@ -165,10 +164,9 @@ public class LauncherScreen extends Screen {
 
     @Override
     public void removed() {
-        client.keyboard.setRepeatEvents(false);
         TooManyBinds.config.launcherX = offsetX;
         TooManyBinds.config.launcherY = offsetY;
-        ((ConfigManager<ModConfig>) AutoConfig.getConfigHolder(ModConfig.class)).save();
+        AutoConfig.getConfigHolder(ModConfig.class).save();
     }
 
     @Override
